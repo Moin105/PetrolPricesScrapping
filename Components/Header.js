15c26d9ed16fafa640c5efc5a36b90e7fcee24e5
moin105 <moinlatif105@gmail.com>
2022-12-28 16:44:21 +0500

    fixes

--- Components/Header.js
+++ Components/Header.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import { useRouter } from 'next/router';
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import logo from '../public/logo.png'
@@ -6,15 +7,18 @@ import Link from 'next/link'
 import {GiHamburgerMenu} from 'react-icons/gi'
 import Nav from './Nav'
 function Header(props) {
+    const router = useRouter();
+  const { pathname } = router;
   const [show ,setShow] = useState(true)
   const navHandle = () => {
-    console.log("$$$$$", nav);
+    console.log("$$$$$", locale);
     setNav(!nav);
   };
   const dataHandle = () => {
     setShow(!show)
     props.setShow(!show);
   };
+  console.log("helos",pathname)
   const [nav, setNav] = useState(false);
   return (
     <>
@@ -24,11 +28,11 @@ function Header(props) {
                 <Image priority src={logo} alt="logo" layout="fill" objectFit="contain"/>
             </figure></Link>
             <ul className={styles.ul}>
-                <Link href="/"><li className={styles.li}><p className={styles.hp}>Petrol prices SG</p>      </li></Link>
-                <Link href="/PetrolPricesMalaysia"><li className={styles.li}><p className={styles.hp}>Petrol prices Malaysia</p></li></Link>
-                <Link href="/TrafficUpdate"><li className={styles.li}><p className={styles.hp}>Traffic Update</p>        </li></Link>
-                <Link href="/COEPrices"><li className={styles.li}><p className={styles.hp}>COE Prices</p>            </li></Link>
-                <Link href="/CarParkingSG"><li className={styles.li}><p className={styles.hp}>Car Parking SG</p>        </li></Link>
+                {pathname == "/" ? <Link href="/"><li className={styles.lis}><p className={styles.hp}>Petrol prices SG</p></li></Link>:<Link href="/"><li className={styles.li}><p className={styles.hp}>Petrol prices SG</p></li></Link>}
+                {pathname.includes("PetrolPricesMalaysia") ? <Link href="/PetrolPricesMalaysia"><li className={styles.lis}><p className={styles.hp}>Petrol prices Malaysia</p></li></Link>:<Link href="/PetrolPricesMalaysia"><li className={styles.li}><p className={styles.hp}>Petrol prices Malaysia</p></li></Link>}
+                {pathname.includes("TrafficUpdate") ? <Link href="/TrafficUpdate"><li className={styles.lis}><p className={styles.hp}>Traffic Update</p></li></Link>:<Link href="/TrafficUpdate"><li className={styles.li}><p className={styles.hp}>Traffic Update</p></li></Link>}
+                {pathname.includes("COEPrices") ? <Link href="/COEPrices"><li className={styles.lis}><p className={styles.hp}>COE Prices</p></li></Link>:<Link href="/COEPrices"><li className={styles.li}><p className={styles.hp}>COE Prices</p></li></Link>}
+                {pathname.includes("CarParkingSG") ? <Link href="/CarParkingSG"><li className={styles.lis}><p className={styles.hp}>Car Parking SG</p>        </li></Link>:<Link href="/CarParkingSG"><li className={styles.li}><p className={styles.hp}>Car Parking SG</p>        </li></Link>}
             </ul>
             <div className={styles.ham}  onClick={()=>{
         navHandle()
--- Components/Headers.js
+++ Components/Headers.js
@@ -1,12 +1,14 @@
 import React,{useState} from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router';
 import styles from '../styles/Home.module.css'
 import logo from '../public/logo.png'
 import Link from 'next/link'
 import Nav from './Nav'
 import {GiHamburgerMenu} from 'react-icons/gi'
 function Headers(props) {
-
+  const router = useRouter();
+  const { pathname } = router;
   const [show ,setShow] = useState(true)
   const navHandle = () => {
     console.log("$$$$$", nav);
@@ -25,11 +27,11 @@ function Headers(props) {
                 <Image priority src={logo} alt="logo" layout="fill" objectFit="contain"/>
             </figure></Link>
             <ul className={styles.ul}>
-                <Link href="/"><li className={styles.li}><p className={styles.hp}>Petrol prices SG</p>      </li></Link>
-                <Link href="/PetrolPricesMalaysia"><li className={styles.li}><p className={styles.hp}>Petrol prices Malaysia</p></li></Link>
-                <Link href="/TrafficUpdate"><li className={styles.li}><p className={styles.hp}>Traffic Update</p>        </li></Link>
-                <Link href="/COEPrices"><li className={styles.li}><p className={styles.hp}>COE Prices</p>            </li></Link>
-                <Link href="/CarParkingSG"><li className={styles.li}><p className={styles.hp}>Car Parking SG</p>        </li></Link>
+            {pathname == "/" ? <Link href="/"><li className={styles.lis}><p className={styles.hp}>Petrol prices SG</p></li></Link>:<Link href="/"><li className={styles.li}><p className={styles.hp}>Petrol prices SG</p></li></Link>}
+                {pathname.includes("PetrolPricesMalaysia") ? <Link href="/PetrolPricesMalaysia"><li className={styles.lis}><p className={styles.hp}>Petrol prices Malaysia</p></li></Link>:<Link href="/PetrolPricesMalaysia"><li className={styles.li}><p className={styles.hp}>Petrol prices Malaysia</p></li></Link>}
+                {pathname.includes("TrafficUpdate") ? <Link href="/TrafficUpdate"><li className={styles.lis}><p className={styles.hp}>Traffic Update</p></li></Link>:<Link href="/TrafficUpdate"><li className={styles.li}><p className={styles.hp}>Traffic Update</p></li></Link>}
+                {pathname.includes("COEPrices") ? <Link href="/COEPrices"><li className={styles.lis}><p className={styles.hp}>COE Prices</p></li></Link>:<Link href="/COEPrices"><li className={styles.li}><p className={styles.hp}>COE Prices</p></li></Link>}
+                {pathname.includes("CarParkingSG") ? <Link href="/CarParkingSG"><li className={styles.lis}><p className={styles.hp}>Car Parking SG</p>        </li></Link>:<Link href="/CarParkingSG"><li className={styles.li}><p className={styles.hp}>Car Parking SG</p>        </li></Link>}
             </ul>
             <div className={styles.ham}  onClick={()=>{
         navHandle()
